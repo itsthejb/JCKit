ece80a509e9797b8b698e07f796d83040eddf968
Jon Crooke <jc@soundcloud.com>
2012-09-17 17:18:12 +0200

    Added some sauces

---
+++ Sources/JCObject.hjc.h
@@ -0,0 +1,13 @@
+//
+//  JCObject.h
+//  JCKit
+//
+//  Created by jc on 17/09/2012.
+//  Copyright (c) 2012 nxtbgthng GmhH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCObject : NSObject
+
+@end
---
+++ Sources/JCObject.mjc.m
@@ -0,0 +1,13 @@
+//
+//  JCObject.m
+//  JCKit
+//
+//  Created by jc on 17/09/2012.
+//  Copyright (c) 2012 nxtbgthng GmhH. All rights reserved.
+//
+
+#import "JCObject.h"
+
+@implementation JCObject
+
+@end
